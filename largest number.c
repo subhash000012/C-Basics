469fe235fd1d113d9fa409a076a3c0577d4ae837
subhash000012 <92287770+subhash000012@users.noreply.github.com>
2021-12-08 09:53:13 +0530

    Add files via upload

---
+++ largest number.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	int a, b;
+	printf("enter your number");
+	scanf("%d%d",&a,&b);
+	if(a>b)
+	{
+	printf("%d is the largest number",a);
+}
+else
+{
+	printf("%d is the largst number ",b);
+}
+}
